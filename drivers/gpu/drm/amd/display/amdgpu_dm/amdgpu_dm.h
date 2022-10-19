1ca9b9f53478c4ebb270080b576643112acb13d8
Alan Liu <HaoPing.Liu@amd.com>
2022-10-19 18:15:14 +0800

    drm/amd/display: Implement multiple secure display
    [Why]
    Current secure display only work with single display, now make it
    work with multiple displays.
    
    [How]
    Create secure_display_context for each crtc instance to store its
    own Region of Interest (ROI) information.
    
    Signed-off-by: Alan Liu <HaoPing.Liu@amd.com>
    Reviewed-by: Wayne Lin <Wayne.Lin@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
@@ -494,11 +494,12 @@ struct amdgpu_display_manager {
 
 #if defined(CONFIG_DRM_AMD_SECURE_DISPLAY)
 	/**
-	 * @crc_rd_wrk:
-	 *
-	 * Work to be executed in a separate thread to communicate with PSP.
-	 */
-	struct crc_rd_work *crc_rd_wrk;
+	 * @secure_display_ctxs:
+ 	 *
+	 * Store the ROI information and the work_struct to command dmub and psp for
+	 * all crtcs.
+ 	 */
+	struct secure_display_context *secure_display_ctxs;
 #endif
 	/**
 	 * @hpd_rx_offload_wq:
