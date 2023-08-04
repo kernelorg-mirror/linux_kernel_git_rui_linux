2325ce421b2c02c27f780137cf82ba00c71390c4
Jiqian Chen <Jiqian.Chen@amd.com>
2023-08-04 18:09:50 +0800

    xen/pci: Add xen_reset_device function
    When device on dom0 side has been reset, the vpci on Xen side
    won't get notification, so the cached state in vpci is all
    out of date with the real device state.
    To solve that problem, this patch add a function to clear all
    vpci device state when reset device on dom0 side.
    
    And call that function in pcistub_init_device. Because when
    we use "pci-assignable-add" to assign a passthrough device in
    Xen, it will reset device and the vpci state will out of date,
    and then device will fail to restore state.
    
    Signed-off-by: Chen Jiqian <Jiqian.Chen@amd.com>
    Signed-off-by: Huang Rui <ray.huang@amd.com>

--- drivers/xen/xen-pciback/pci_stub.c
+++ drivers/xen/xen-pciback/pci_stub.c
@@ -421,6 +421,7 @@ static int pcistub_init_device(struct pci_dev *dev)
 	else {
 		dev_dbg(&dev->dev, "resetting (FLR, D3, etc) the device\n");
 		__pci_reset_function_locked(dev);
+		xen_reset_device(&dev->dev);
 		pci_restore_state(dev);
 	}
 	/* Now disable the device (this also ensures some private device
